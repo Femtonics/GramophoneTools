d6d559ba398accb1a1f1f768f82f0de2a098a9dc
Horváth Gergely <ghorvath@femtonics.eu>
2018-04-26 18:18:10 +0200

    removed 'style_external_links'

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -94,7 +94,6 @@ html_theme_options = {
     'logo_only': False,
     'display_version': True,
     'prev_next_buttons_location': None,
-    'style_external_links': True,
     'vcs_pageview_mode': '',
     # Toc options
     'collapse_navigation': False,
