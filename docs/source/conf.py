57951753281aacc0931d3bfbd5e22ac01260ff21
Horváth Gergely <ghorvath@femtonics.eu>
2018-04-27 13:09:19 +0200

    RTD setup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,6 +19,7 @@ sys.path.insert(0, os.path.abspath('../../GramophoneTools/Recorder'))
 sys.path.insert(0, os.path.abspath('../../GramophoneTools/LinMaze'))
 sys.path.insert(0, os.path.abspath('../../GramophoneTools/IO'))
 from ver import __version__ as g_ver
+import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -83,7 +84,7 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
