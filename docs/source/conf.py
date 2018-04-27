8f185b5203cc611bb0311a380265bb26a3cd7768
Horváth Gergely <ghorvath@femtonics.eu>
2018-04-27 13:15:57 +0200

    Don't use includehidden

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -101,7 +101,7 @@ html_theme_options = {
     'collapse_navigation': False,
     'sticky_navigation': True,
     'navigation_depth': 4,
-    'includehidden': True,
+    # 'includehidden': True,
     'titles_only': False
 }
 
