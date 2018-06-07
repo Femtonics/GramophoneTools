12cf303d6d5dd13e02d9adc68692990b8a74066f
horvathge <ghorvath@femtonics.eu>
2018-06-07 17:17:28 +0200

    Linted

--- GramophoneTools/ver.py
+++ GramophoneTools/ver.py
@@ -1,5 +1,8 @@
+""" Simple module for single sourcing the version. """
+
 __version__ = '0.4.3'
 
+
 def print_version():
-    global __version__
+    """ Prints the version. Called by the command line function 'gramver'. """
     print('Gramophone Tools version:', __version__)
