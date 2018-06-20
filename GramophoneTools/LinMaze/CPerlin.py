96d30edfacb6810a2604a7cf1bdfbbe9bf4976b7
Horváth Gergely <horvath.gergely@FEMTONICS.LOCAL>
2018-06-20 17:33:51 +0200

    Marble, Wood and Cloud functions

--- GramophoneTools/LinMaze/CPerlin.py
+++ GramophoneTools/LinMaze/CPerlin.py
@@ -1,4 +1,5 @@
 import ctypes
+import math
 import os
 
 DIR = os.path.dirname(__file__)
@@ -15,25 +16,48 @@ def perlin2d(x, y, freq, depth):
     return perlin_lib.perlin2d(c_X, c_Y, c_freq, c_depth)
 
 
+def cloud(width, height):
+    img = np.zeros((width, height))
+    for index, _ in np.ndenumerate(img):
+        x, y = index[0], index[1]
+        img[x][y] = int(255*perlin2d(x, y, 1/70, 5))
+    return np.transpose(img)
+
+
+def marble(width, height):
+    img = np.zeros((width, height))
+    for index, _ in np.ndenumerate(img):
+        x, y = index[0], index[1]
+        img[x][y] = perlin2d(x, y, 1/70, 5)
+        img[x][y] = int(255*(math.sin(16 * x * 1/width + 4 * (img[x][y] - 0.5)) + 1) * 0.5)
+
+    return np.transpose(img)
+
+
+def wood(width, height):
+    img = np.zeros((width, height))
+    for index, _ in np.ndenumerate(img):
+        x, y = index[0], index[1]
+        g = perlin2d(x, y, 1/150, 1) * 13
+        img[x][y] = 255*(g-int(g))
+    return np.transpose(img)
+
+
 if __name__ == '__main__':
     import numpy as np
     from PIL import Image
     import time
-    start=time.time()
-    res = (400, 400)
+    start = time.time()
 
-    img = np.zeros(res)
-    for x in range(res[0]):
-        for y in range(res[1]):
-            # print(x,y)
-            # img[x][y] = int(255*perlin2d(x, y, 1/70, 5))
-            g = perlin2d(x,y, 1/150, 1) * 13
-            img[x][y] = 255*(g-int(g))
+    cloud_img = Image.fromarray(cloud(800, 600))
+    marble_img = Image.fromarray(marble(800, 600))
+    wood_img = Image.fromarray(wood(800, 600))
 
-
-    img = Image.fromarray(np.transpose(img))
-    # imt = img.convert('RGB')
-    # img.convert('RGB').save('wood.png')
-    img.convert('RGB').show()
     print('done', str(time.time()-start))
-    # img.show()
+
+    cloud_img.convert('RGB').show()
+    marble_img.convert('RGB').show()
+    wood_img.convert('RGB').show()
+
+    # cloud_img.convert('RGB').save('cloud.png')
+    # wood.convert('RGB').save('wood.png')
