8c885499abc2d9b88a6fb9a16703e809291f5e87
Horváth Gergely <horvath.gergely@FEMTONICS.LOCAL>
2018-07-31 10:20:12 +0200

    Create windows installers

--- publish.bat
+++ publish.bat
@@ -1,4 +1,7 @@
 call .\docs\make.bat html
 python setup.py sdist
 python setup.py bdist_wheel
+python setup.py bdist_msi
+python setup.py bdist_wininst --bitmap "res\femtonics_installer.bmp"
+python setup.py clean --all
 twine upload dist/*
